30c72b6a8fc286e1a17ceb41595f2cdae4098643
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2025-04-05 13:00:00 -0300

    Delete projeto directory

--- projeto/projeto/src/main/java/com/senai/projeto/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senai.projeto.repository;
-
-import com.senai.projeto.model.UsuarioEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
-}
